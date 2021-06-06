c2b96c3315c8c102ec0365628135a4cf8c9636c2
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-06-06 17:10:21 +0300

    solved Lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <span>Made by: Anas Al-Ramahi</span>
+            </div>
+        )
+    }
+}
+
+export default Footer;
